bddbe03aa3e4aca3cc707d0a2ea91ca5f24ab9d0
liurongqiang <liurongqianghello@163.com>
2021-11-03 22:38:46 +0800

    完善工程

--- multipledatasource/src/main/java/com/liurq/multidatasource/core/AbstractRouting.java
+++ multipledatasource/src/main/java/com/liurq/multidatasource/core/AbstractRouting.java
@@ -1,6 +1,8 @@
 package com.liurq.multidatasource.core;
 
+import com.liurq.multidatasource.enumuration.RoutingStategyEnum;
 import com.liurq.multidatasource.support.DsRoutingSetProperties;
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingUse;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
@@ -40,16 +42,70 @@ public abstract class AbstractRouting implements IRouting, InitializingBean {
      */
     @Override
     public String getFormatTableSuffix(Integer tableIndex) {
-        return null;
+
+        StringBuffer stringBuffer = new StringBuffer(dsRoutingSetProperties.getTableSuffixConnect());
+
+        try {
+            stringBuffer.append(String.format(getDsRoutingSetProperties().getTableSuffixStyle(),tableIndex));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return stringBuffer.toString();
     }
 
     /**
-     * 配置检查
+     * 工程启动的时候，进行配置检查
      *
      * @throws Exception
      */
     @Override
     public void afterPropertiesSet() throws Exception {
 
+        switch (getDsRoutingSetProperties().getRoutingStategy()) {
+            case "ROUTING_DS_TABLE_STATEGY":
+                checkRoutingDsTableStategyConfig();
+                break;
+            case "ROUTING_DS_STATEGY":
+                checkRoutingDsStategyConfig();
+                break;
+            case "ROUTING_TABLE_STATEGY":
+                checkRoutingTableStategyConfig();
+                break;
+        }
+
     }
+
+    /**
+     * 多库多表配置检查
+     */
+    private void checkRoutingDsTableStategyConfig() {
+
+        if (dsRoutingSetProperties.getTableNum() <= 1
+                || dsRoutingSetProperties.getDataSourceNum() <= 1) {
+            log.error("多库多表配置项有误.");
+        }
+
+    }
+
+    /**
+     * 多库一表配置检查
+     */
+    private void checkRoutingDsStategyConfig() {
+        if (dsRoutingSetProperties.getTableNum() != 1
+                || dsRoutingSetProperties.getDataSourceNum() <= 1) {
+            log.error("多库一表配置项有误.");
+        }
+    }
+
+    /**
+     * 一库多表配置检查
+     */
+    private void checkRoutingTableStategyConfig() {
+        if (dsRoutingSetProperties.getTableNum() <= 1
+                || dsRoutingSetProperties.getDataSourceNum() != 1) {
+            log.error("一库多表配置项有误.");
+        }
+    }
+
 }
