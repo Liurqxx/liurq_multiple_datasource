a6331827828f880acb253f72ea2c05ceee1a6dbe
liurongqiang <liurongqianghello@163.com>
2021-11-03 22:17:10 +0800

    完善工程

--- multipledatasource/src/main/java/com/liurq/multidatasource/config/DataSourceConfig.java
+++ multipledatasource/src/main/java/com/liurq/multidatasource/config/DataSourceConfig.java
@@ -5,12 +5,18 @@ import com.liurq.multidatasource.dynamicdatasource.LiurqMultiDataSource;
 import com.liurq.multidatasource.support.DsRoutingSetProperties;
 import com.liurq.multidatasource.support.MultipleDruidProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -83,8 +89,33 @@ public class DataSourceConfig {
         //设置默认数据源
         liurqMultiDataSource.setDefaultTargetDataSource(dataSource00());
 
+        //设置所有数据库
+        HashMap<Integer, String> setMappings = new HashMap<>();
+        setMappings.put(0, "dataSource00");
+        setMappings.put(1, "dataSource01");
+        setMappings.put(2, "dataSource02");
+        dsRoutingSetProperties.setDataSourceKeysMapping(setMappings);
 
+        return liurqMultiDataSource;
     }
 
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("liurqMultiDataSource") LiurqMultiDataSource liurqMultiDataSource) throws Exception {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(liurqMultiDataSource);
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResource(""));
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(@Qualifier("liurqMultiDataSource") LiurqMultiDataSource liurqMultiDataSource) {
+        return new DataSourceTransactionManager(liurqMultiDataSource);
+    }
 
 }
