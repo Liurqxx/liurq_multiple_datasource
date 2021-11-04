506b85ffda4f4a92841976db9ef15d77fd00d585
liurongqiang <liurongqianghello@163.com>
2021-11-04 21:24:22 +0800

    完善工程

--- multipledatasource/src/main/java/com/liurq/multidatasource/annotation/Router.java
+++ multipledatasource/src/main/java/com/liurq/multidatasource/annotation/Router.java
@@ -1,11 +1,20 @@
 package com.liurq.multidatasource.annotation;
 
+import com.liurq.multidatasource.constant.RoutingStategyConstant;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author liurq
  * @desc
  * @date 2021-11-04  21:18
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Router {
 
-    String routingFiled = "";
+    String routingFiled() default RoutingStategyConstant.DEFAULT_ROUTING_FIELD;
 }
---
+++ multipledatasource/src/main/java/com/liurq/multidatasource/constant/RoutingStategyConstant.java
@@ -0,0 +1,21 @@
+package com.liurq.multidatasource.constant;
+
+/**
+ * @author liurq
+ * @desc
+ * @date 2021-11-04  21:20
+ */
+public class RoutingStategyConstant {
+
+    /**
+     * 表策略
+     */
+    public static final String ROUTING_DS_TABLE_STATEGY = "ROUTING_DS_TABLE_STATEGY";
+    public static final String ROUTING_DS_STATEGY = "ROUTING_DS_STATEGY";
+    public static final String ROUTING_TABLE_STATEGY = "ROUTING_TABLE_STATEGY";
+
+    /**
+     * 路由字段
+     */
+    public static final String DEFAULT_ROUTING_FIELD = "orderId";
+}
